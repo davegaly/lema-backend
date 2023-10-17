5a693c1c7b213545924fbf869dc0fa066cd42d6d
davegaly <davegaly@gmail.com>
2023-10-17 21:28:21 +0200

    added dept save

--- departments.js
+++ departments.js
@@ -11,13 +11,6 @@ const routerDepartments = new Router({
 //dbFunctions.insertDepartment({name:'HR'});
 //dbFunctions.insertDepartment({name:'Business Sintesi'});
 
-let departmentsRecords = [
-  { id: 100, iname: 'Quartz Analog Wrist Watch', price: 'US $4.99' },
-  { id: 101, iname: 'Leather Peep Pump Heels', price: 'US $33.56' },
-  { id: 102, iname: 'Apple iPod', price: 'US $219.99' },
-  { id: 103, iname: 'Prince Phantom 97P Tennnis Racket', price: 'US $50.00' },
-];
-
 // Routes
 routerDepartments.get('/getbyid/:id', async (ctx, next) => {
   await new Promise((resolve, reject) => {
@@ -37,8 +30,9 @@ routerDepartments.get('/list', async (ctx, next) => {
   });  
 });
 
-/*
-router.post('/add', (ctx, next) => {
+routerDepartments.post('/save', (ctx, next) => {
+  console.log(ctx.request.body);
+  /*
   let newItem = items.push({
     id: ctx.request.body.id,
     iname: ctx.request.body.iname,
@@ -48,8 +42,9 @@ router.post('/add', (ctx, next) => {
 		ctx.body = `New item = added with id: ${ctx.request.body.id} & item name: ${
 			ctx.request.body.iname
 		}`;
+    */
 	next();
 });
-*/
+
 
 module.exports = routerDepartments;
