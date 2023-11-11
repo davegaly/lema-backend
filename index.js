52254cb59db3ac0892df1d07d330615134a3c286
davegaly <davegaly@gmail.com>
2023-11-11 17:54:32 +0100

    added check for .env variables

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const Koa = require('koa');
-const logger = require('koa-logger');
+require("dotenv").config();
+const Koa = require("koa");
+const logger = require("koa-logger");
 const cors = require("@koa/cors");
-const { koaBody } = require('koa-body');
+const { koaBody } = require("koa-body");
 
-//const departmentsProvider = require('./db/providers/departmentsProvider.js');
+console.log("App is starting");
+console.log("");
+
+console.log("Checking mandatory .env settings");
+const settingsServerENV = process.env["SERVER_ENV"];
+console.log("SERVER_ENV: " + settingsServerENV);
+if (settingsServerENV == undefined) {
+  console.log("Could not retrieve SERVER_END in .env file. Shutting down...");
+  return;
+}
 
 const app = new Koa();
 
@@ -14,22 +24,22 @@ app.use(cors());
 app.use(koaBody());
 
 const helloWorld = (ctx) => {
-  ctx.body = 'Hello World!!!';
+  ctx.body = "Hello World!!!";
 };
 
 const dateTest = (ctx) => {
-  ctx.body = 'Dave test rcamadonn';
+  ctx.body = "Dave test rcamadonn";
 };
 
 //router.get('/', helloWorld);
 //router.get('/davetesta', dateTest);
 
-let departmentsAPI = require('./api/departmentsAPI.js');
-let teamsAPI = require('./api/teamsAPI.js');
-let employeesAPI = require('./api/employeesAPI.js');
-let employeesTeamsAPI = require('./api/employeesTeamsAPI.js');
-let usersAPI = require('./api/usersAPI.js');
-let testAPI = require('./api/testAPI.js');
+let departmentsAPI = require("./api/departmentsAPI.js");
+let teamsAPI = require("./api/teamsAPI.js");
+let employeesAPI = require("./api/employeesAPI.js");
+let employeesTeamsAPI = require("./api/employeesTeamsAPI.js");
+let usersAPI = require("./api/usersAPI.js");
+let testAPI = require("./api/testAPI.js");
 
 //app.use(router.routes());
 app.use(departmentsAPI.routes());
@@ -39,10 +49,10 @@ app.use(employeesTeamsAPI.routes());
 app.use(usersAPI.routes());
 app.use(testAPI.routes());
 
-
 // testers
 //const departmentsProvider = require('./db/providers/departmentsProvider.js');
 //departmentsProvider.save({"id":0, "name":"Business Compagnie"}, function(){});
 
+console.log("All good. All ready. Fire some API to see things.");
 
 app.listen(3000);
